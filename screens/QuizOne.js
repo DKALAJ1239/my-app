be4a40cb7d45188c15d63374056f6c328db3cfea
dkalaj1239 <domkalaj1239@gmail.com>
2023-04-23 00:57:54 -0400

    restarting

--- screens/QuizOne.js
+++ screens/QuizOne.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import React from "react";
-// import Questions from "../slices/Questions";
+import Questions from "../slices/Questions";
 
 const QuizOne = () => {
-//   const data = Questions;
-//   const currentQuestion = data[0];
+  const data = Questions;
+  const currentQuestion = data[0];
 
   return (
     <SafeAreaView>
