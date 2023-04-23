80c39044a23cb6af3ecf98178dac1643eda3468f
dkalaj1239 <domkalaj1239@gmail.com>
2023-04-23 01:18:21 -0400

    fixed minor bugs

--- screens/QuizOne.js
+++ screens/QuizOne.js
@@ -1,17 +1,37 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import React from "react";
 import Questions from "../slices/Questions";
+import { CheckBox } from "react-native-elements";
 
 const QuizOne = () => {
   const data = Questions;
-  const currentQuestion = data[0];
+  const [answer, setAnswer] = useState();
+  // const currentQuestion = data[0];
 
   return (
-    <SafeAreaView>
-      <Text>
-        QuizScreenOne
-      </Text>
-    </SafeAreaView>
+    <View>
+      <Text style={styles.question}>{question}</Text>
+      <View>
+        {answers.map((answer) => (
+          <CheckBox
+            center
+            key={answer.id}
+            title={answer.choice}
+            // if these are not empty strings, the default checkbox appears
+            checkedIcon=""
+            uncheckedIcon=""
+            checked={this.state[answer.id] || false}
+            // ternary conditionally renders the color of choice container
+            containerStyle={
+              this.state[answer.id]
+                ? { backgroundColor: answer.isCorrect ? "lightgreen" : "pink" }
+                : null
+            }
+            onPress={() => this.handleSelection(answer)}
+          />
+        ))}
+      </View>
+    </View>
   );
 };
 
