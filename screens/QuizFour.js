744603c995e9e26469de215a496f1cd2595281e8
dkalaj1239 <domkalaj1239@gmail.com>
2023-04-25 04:06:58 -0400

    Fixing the google map quiz

--- screens/QuizFour.js
+++ screens/QuizFour.js
@@ -1,19 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import Axios from "axios";
 
 const QuizQuestions = [
   {
-    question: "What is the capital of France?",
-    answer: "Paris",
-    lat: 48.8584,
-    lng: 2.2945,
+    question: "Guess the Country in the Picture",
+    lat: 48.8566,
+    lng: 2.3522,
+    options: [
+      { id: "0", option: "A", answer: "France" },
+      { id: "1", option: "B", answer: "Russia" },
+      { id: "2", option: "C", answer: "Britain" },
+      { id: "3", option: "D", answer: "Bosnia" },
+    ],
+    correctAnswerIndex: 0,
   },
   {
-    question: "What is the tallest mountain in the world?",
-    answer: "Mount Everest",
-    lat: 128.9881,
-    lng: 86.9253,
+    question: "Guess the Country in the Picture",
+    lat: 52.518589,
+    lng: 13.376665,
+    options: [
+      {
+        id: "0",
+        option: "A",
+        answer: "Ukraine",
+      },
+      {
+        id: "1",
+        option: "B",
+        answer: "Germany",
+      },
+      {
+        id: "2",
+        option: "C",
+        answer: "Iceland",
+      },
+      {
+        id: "3",
+        option: "D",
+        answer: "Greenland",
+      },
+    ],
+    correctAnswerIndex: 1,
   },
   // add more questions and answers here
 ];
@@ -22,9 +50,29 @@ const API_KEY = "AIzaSyBicqUIzS6qIzkk8vtr2bt5kZMlQpcMxW4";
 
 const QuizFour = ({ navigation }) => {
   const [imageUrl, setImageUrl] = useState("");
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [score, setScore] = useState(0);
+
+  const handleAnswer = (answer) => {
+    if (
+      answer ===
+      QuizQuestions[currentQuestion].options[
+        QuizQuestions[currentQuestion].correctAnswerIndex
+      ].answer
+    ) {
+      setScore(score + 1);
+    }
+    if (currentQuestion === QuizQuestions.length - 1) {
+      navigation.navigate("Results", { score });
+    } else {
+      setCurrentQuestion(currentQuestion + 1);
+      setSelectedOption(null);
+    }
+  };
 
   useEffect(() => {
-    const { lat, lng } = QuizQuestions[0];
+    const { lat, lng } = QuizQuestions[currentQuestion];
     const url = `https://maps.googleapis.com/maps/api/streetview?size=600x300&location=${lat},${lng}&key=${API_KEY}`;
 
     Axios.get(url)
@@ -34,29 +82,69 @@ const QuizFour = ({ navigation }) => {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [currentQuestion]);
 
   return (
     <View style={styles.container}>
+      <Text style={styles.question}>
+        {QuizQuestions[currentQuestion].question}
+      </Text>
       {imageUrl ? (
         <Image style={styles.image} source={{ uri: imageUrl }} />
       ) : (
         <Text>No image available</Text>
       )}
+      {QuizQuestions[currentQuestion].options.map((option) => (
+        <TouchableOpacity
+          key={option.id}
+          style={styles.button}
+          onPress={() => handleAnswer(option.answer)}
+        >
+          <Text style={styles.buttonText}>
+            {option.option} - {option.answer}
+          </Text>
+        </TouchableOpacity>
+      ))}
+      <Text style={styles.score}>Score: {score}</Text>
     </View>
   );
 };
 
+export default QuizFour;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#F8F8F8",
+  },
+  question: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginVertical: 20,
   },
   image: {
-    width: 600,
-    height: 300,
+    width: 300,
+    height: 150,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#4CAF50",
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
+    minWidth: 200,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 18,
+    textAlign: "center",
+  },
+  score: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginTop: 20,
   },
 });
-
-export default QuizFour;
--- slices/QuestionsQ4.js
+++ slices/QuestionsQ4.js
@@ -1,17 +1,89 @@
 const quizQuestions = [
   {
-    question: "What is the capital of France?",
-    answer: "Paris",
+    question: "Guess the Country in the Picture",
     lat: 48.8566,
     lng: 2.3522,
+    options: [
+      {
+        id: "0",
+        option: "A",
+        answer: "France",
+      },
+      {
+        id: "1",
+        option: "B",
+        answer: "Russia",
+      },
+      {
+        id: "2",
+        option: "C",
+        answer: "Britian",
+      },
+      {
+        id: "3",
+        option: "D",
+        answer: "Bosnia",
+      },
+    ],
+    correctAnswerIndex: 0,
   },
   {
-    question: "What is the tallest mountain in the world?",
-    answer: "Mount Everest",
-    lat: 27.9881,
-    lng: 86.9253,
+    question: "Guess the Country in the Picture",
+    lat: 52.518589,
+    lng: 13.376665,
+    options: [
+      {
+        id: "0",
+        option: "A",
+        answer: "Ukraine",
+      },
+      {
+        id: "1",
+        option: "B",
+        answer: "Germany",
+      },
+      {
+        id: "2",
+        option: "C",
+        answer: "Iceland",
+      },
+      {
+        id: "3",
+        option: "D",
+        answer: "Greenland",
+      },
+    ],
+    correctAnswerIndex: 1,
   },
   // add more questions and answers here
 ];
 
 export default quizQuestions;
+
+//   {
+//     prompt: "Which country is labeled as #10?",
+//     imageUri: "https://www.funtrivia.com/qphotos/87/408487_0_o.jpg",
+//     options: [
+//       {
+//         id: "0",
+//         option: "A",
+//         answer: "Philippines",
+//       },
+//       {
+//         id: "1",
+//         option: "B",
+//         answer: "Indonesia",
+//       },
+//       {
+//         id: "2",
+//         option: "C",
+//         answer: "Cambodia",
+//       },
+//       {
+//         id: "3",
+//         option: "D",
+//         answer: "Malaysia",
+//       },
+//     ],
+//     correctAnswerIndex: 0,
+//   },
